4bb0b4b437556e793392ef8f1822647d032796cf
sarakhandaker <kttsara@gmail.com>
2021-02-22 10:56:00 -0800

    added initial migration

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,7 +36,7 @@ namespace API
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            })
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
