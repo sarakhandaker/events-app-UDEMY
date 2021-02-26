f1d48fab8c3038b1a95e85acb7138c97c334f5d4
sarakhandaker <kttsara@gmail.com>
2021-02-26 09:59:00 -0800

    moved mediator to base api controller

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,11 +1,17 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class BaseApiController: ControllerBase
+    public class BaseApiController : ControllerBase
     {
-        
+        private IMediator _mediator;
+        //protects means its avaliable to derived classes
+        // ??= if the left side is null then use the right side
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
     }
 }
