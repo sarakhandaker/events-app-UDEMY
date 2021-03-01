5991cea80b2bb56ddf5cd79dcfc4d8acb025059c
sarakhandaker <kttsara@gmail.com>
2021-03-01 14:30:47 -0800

    added select/cancel selected activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,17 +7,32 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined)
+
   useEffect(() => {
     axios.get<Activity[]>("http://localhost:5000/api/activities").then(resp => {
       setActivities(resp.data)
     }
     )
   }, [])
+
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(act => act.id === id));
+  }
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined)
+  }
+
   return (
     <>
       <NavBar />
-      <Container style={{'margin-top': '7em'}}>
-        <ActivityDashboard activities={activities}/>
+      <Container style={{ 'margin-top': '7em' }}>
+        <ActivityDashboard
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          activities={activities}
+        />
       </Container>
     </>
   );
