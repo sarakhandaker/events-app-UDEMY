adebfbfda62dc02babb611915704140b6424c6b4
sarakhandaker <kttsara@gmail.com>
2021-03-02 11:42:50 -0800

    setting up agent

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { v4 as uuid } from 'uuid';
+import agent from '../api/agent'
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
@@ -12,10 +12,7 @@ function App() {
   const [editMode, setEditMode] = useState(false)
 
   useEffect(() => {
-    axios.get<Activity[]>("http://localhost:5000/api/activities").then(resp => {
-      setActivities(resp.data)
-    }
-    )
+    agent.Activities.list().then(resp => setActivities(resp))
   }, [])
 
   function handleSelectActivity(id: string) {
