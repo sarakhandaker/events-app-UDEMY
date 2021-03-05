cb8c6502147940d59424fa1b2b76e69a0fb83404
sarakhandaker <kttsara@gmail.com>
2021-03-04 16:50:52 -0800

    mobx strict mode

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,30 +1,34 @@
-import {makeAutoObservable } from "mobx"
-import agent from "../api/agent"
-import { Activity } from "../models/activity"
+import { makeAutoObservable } from "mobx";
+import agent from "../api/agent";
+import { Activity } from "../models/activity";
 
 export default class ActivityStore {
-    activities: Activity[] = []
-    selectedActivity: Activity | null = null
-    editMode= false
-    loading = false
-    initialLoading = false
-    
-    constructor(){
-        makeAutoObservable(this)
-    }
+  activities: Activity[] = [];
+  selectedActivity: Activity | null = null;
+  editMode = false;
+  loading = false;
+  initialLoading = false;
 
-    loadActivities= async ()=> {
-        this.initialLoading = true
-        try {
-            const activities= await agent.Activities.list();
-            activities.forEach(act => {
-                act.date=act.date.split('T')[0]
-                this.activities.push(act)
-            })
-            this.initialLoading = false
-        }catch(error) {
-            console.log(error)
-            this.initialLoading = false
-        }
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  loadActivities = async () => {
+    this.setLoadingInitial(true);
+    try {
+      const activities = await agent.Activities.list();
+      activities.forEach((act) => {
+        act.date = act.date.split("T")[0];
+        this.activities.push(act);
+      });
+      this.setLoadingInitial(false);
+    } catch (error) {
+      console.log(error);
+      this.setLoadingInitial(false);
     }
+  };
+
+  setLoadingInitial = (state: boolean) => {
+    this.initialLoading = state;
+  };
 }
