a2e5a20b97ddd6563ad7f208af65240bcbb71c75
sarakhandaker <kttsara@gmail.com>
2021-03-10 10:51:59 -0800

    details activity api error handler

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +13,16 @@ namespace API.Controllers
         //protects means its avaliable to derived classes
         // ??= if the left side is null then use the right side
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result == null) return NotFound();
+            if (result.IsSuccess && result.Value != null)
+                return Ok(result.Value);
+            if (result.IsSuccess && result.Value == null)
+                return NotFound();
+            return BadRequest(result.Error);
+        }
 
     }
 }
