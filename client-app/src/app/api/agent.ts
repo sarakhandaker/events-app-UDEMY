12adfb1c0220ad9d5ef5dc094a5c4cc6772d5d21
sarakhandaker <kttsara@gmail.com>
2021-03-15 13:14:55 -0700

    handling 500 error

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { Activity } from '../models/activity';
 import { toast } from 'react-toastify';
 import { history } from '../..';
+import { store } from '../stores/store';
 
 const sleep = (delay: number) => {
     return new Promise(resolve => {
