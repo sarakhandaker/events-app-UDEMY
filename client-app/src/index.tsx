2b67ff5482381e0df1dbdd038f693e4a1f194509
sarakhandaker <kttsara@gmail.com>
2021-03-23 13:42:31 -0700

    date input field added

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,6 +5,7 @@ import App from './app/layout/App';
 import 'semantic-ui-css/semantic.min.css'
 import 'react-calendar/dist/Calendar.css'
 import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { Router } from 'react-router-dom';
